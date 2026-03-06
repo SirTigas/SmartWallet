0556c3905e6a05a40adac634dc9bf55ee40c3001
SirTigas <tiagocardoso2077@gmail.com>
2026-03-06 18:22:30 -0300

    first commit Smart Wallet

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    I'm here
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+import './bootstrap';
+import  '../css/app.css';
+
+import { createApp, h } from 'vue'
+import {createInertiaApp, Head, Link} from '@inertiajs/vue3'
+
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .component('Head', Head)
+            .component('Link', Link)
+            .use(plugin)
+            .mount(el)
+    },
+})
